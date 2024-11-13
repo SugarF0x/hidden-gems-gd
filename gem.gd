c57e7ad2e23c902322ad3e2003b42e24f8c21738
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-11-13 15:04:45 +0300

    refactor gem
    remove implicit icon randomization and cleanup the code itself

--- gem.gd
+++ gem.gd
@@ -1,7 +1,9 @@
+@tool
 class_name Gem extends Control
 
+#region static
+
 const ICONS_PATH := "res://assets/gems"
-const SIZE := 100
 
 enum BackgroundState {
 	EMPTY,
@@ -22,28 +24,32 @@ const BACKGROUND_IMAGES: Dictionary = {
 @onready var background_texture_rect: TextureRect = $BackgroundTextureRect
 @onready var gem_texture_rect: TextureRect = $GemTextureRect
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var button: Button = $Button
 
-var disabled: bool = false
-func disable() -> void: disabled = true
-func enable() -> void: disabled = false
+#endregion
+#region properties
 
-@export var state := BackgroundState.HIDDEN:
-	set(value):
-		state = value
-		update_background_image()
-		update_gem_visibility()
+@export var state: BackgroundState = BackgroundState.HIDDEN : set = set_state
+@export var icon: Texture2D = preload(ICONS_PATH + "/1.svg") : set = set_icon
+@export var tile_size: int = 100 : set = set_tile_size
 
-var selected_icon: Texture2D:
-	set(value):
-		selected_icon = value
-		update_gem_texture()
-
-func _ready() -> void:
-	set_controls_node_size()
-	set_random_gem_icon()
+func set_state(value: BackgroundState) -> void:
+	state = value
 	update_background_image()
 	update_gem_visibility()
-	_editor_on_ready()
+
+func set_icon(value: Texture2D) -> void:
+	icon = value
+	update_gem_texture()
+
+func set_tile_size(value: int) -> void:
+	tile_size = value
+	update_node_sizes()
+
+#endregion
+
+func _ready() -> void:
+	connect_button_signals()
 
 func set_random_gem_icon() -> void:
 	var dir: DirAccess = DirAccess.open(ICONS_PATH)
@@ -67,27 +73,27 @@ func set_random_gem_icon() -> void:
 		return
 
 	var random_index: int = randi() % svg_files.size()
-	selected_icon = load(ICONS_PATH + "/" + svg_files[random_index])
+	icon = load(ICONS_PATH + "/" + svg_files[random_index])
 
-func set_controls_node_size() -> void:
-	custom_minimum_size = Vector2(SIZE, SIZE)
+func update_node_sizes() -> void:
+	if not is_node_ready(): return
+	custom_minimum_size = Vector2(tile_size, tile_size)
+	pivot_offset = custom_minimum_size / 2
+	gem_texture_rect.size = Vector2(tile_size * .69, tile_size * .68)
+	gem_texture_rect.position = Vector2((tile_size * .31) / 2, (tile_size * .32) / 2)
 
 func update_background_image() -> void:
-	if not background_texture_rect: return
+	if not is_node_ready(): return
 	background_texture_rect.texture = BACKGROUND_IMAGES[state]
 
 func update_gem_texture() -> void:
-	if not gem_texture_rect: return
-	gem_texture_rect.texture = selected_icon
+	if not is_node_ready(): return
+	gem_texture_rect.texture = icon
 
 func update_gem_visibility() -> void:
-	if not gem_texture_rect: return
+	if not is_node_ready(): return
 	if state == BackgroundState.FOUND or state == BackgroundState.MISSED: gem_texture_rect.visible = true
 	else: gem_texture_rect.visible = false
-
-func _editor_on_ready() -> void:
-	if Engine.is_editor_hint(): state = BackgroundState.FOUND
-	else: state = BackgroundState.HIDDEN
 
 func set_opacity(value: float) -> void: modulate.a = value
 
@@ -101,13 +107,28 @@ func play_press(value: bool) -> Signal:
 	else: animation_player.play_backwards("press")
 	return animation_player.animation_finished
 
+#region press handling	
+
+func connect_button_signals() -> void:
+	button.button_down.connect(on_press_down)
+	button.button_up.connect(on_press_up)
+	button.pressed.connect(on_gem_clicked)
+
+var disabled: bool = false
+func disable() -> void: disabled = true
+func enable() -> void: disabled = false
+
 signal gem_clicked
 func on_gem_clicked(): if not disabled: gem_clicked.emit()
 
 var _is_pressed: bool = false
-func on_press_down(): if not disabled:
-	_is_pressed = true
-	play_press(true)
-func on_press_up(): if not disabled || _is_pressed:
-	_is_pressed = false
-	play_press(false)
+func on_press_down():
+	if not disabled:
+		_is_pressed = true
+		play_press(true)
+func on_press_up():
+	if not disabled || _is_pressed:
+		_is_pressed = false
+		play_press(false)
+
+#endregion
