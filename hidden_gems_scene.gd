72b214af96185ce667baed3e1904247157327149
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-11-15 19:11:30 +0300

    add option to skip intro

--- hidden_gems_scene.gd
+++ hidden_gems_scene.gd
@@ -3,6 +3,11 @@ extends Node
 @onready var game_scene: GameScene = $UI/GameScene
 @onready var countdown: Countdown = $UI/Countdown
 
+@export var skip_intro: bool = true
+
 func _ready() -> void:
-	#await countdown.play()
-	game_scene.start_game.call_deferred()
+	if skip_intro:
+		game_scene.start_game.call_deferred()
+	else:
+		await countdown.play()
+		game_scene.start_game()
