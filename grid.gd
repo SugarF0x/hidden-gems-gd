de17adea82475e6555fe793a01d76663f81aed4a
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-11-15 15:56:08 +0300

    add grid reveal states

--- grid.gd
+++ grid.gd
@@ -5,6 +5,13 @@ class_name GemGrid extends Control
 
 var gem_scene: PackedScene = preload("res://gem.tscn")
 
+enum RevealType {
+	NONE,
+	ALL,
+	SELECTED,
+	SELECTED_AND_CORRECT,
+}
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var panel: PanelContainer = $Panel
 @onready var margin_container: MarginContainer = $Panel/MarginContainer
@@ -14,8 +21,9 @@ var gem_scene: PackedScene = preload("res://gem.tscn")
 #region properties
 
 @export var grid_size: Vector2i = Vector2i(2,3) : set = set_grid_size
-@export var gems_revealed: bool = false : set = set_gems_revealed
+@export var reveal_type: RevealType = RevealType.ALL : set = set_reveal_type
 @export var correct_gem_indexes: Array[int] = [0, 3] : set = set_correct_gem_indexes
+@export var selected_gem_indexes: Array[int] = [] : set = set_selected_gem_indexes
 
 var tile_size: int = Gem.max_tile_size : set = set_tile_size
 
@@ -23,17 +31,17 @@ func set_grid_size(value: Vector2i) -> void:
 	grid_size = value
 	update_nodes()
 
-func set_gems_revealed(value: bool) -> void:
-	gems_revealed = value
-	
-	for index in gems.size(): 
-		var gem = gems[index]
-		if value: gem.state = Gem.GemState.FOUND if index in correct_gem_indexes else Gem.GemState.EMPTY
-		else: gem.state = Gem.GemState.HIDDEN
+func set_reveal_type(value: RevealType) -> void:
+	reveal_type = value
+	update_gem_reveal_state()
 
 func set_correct_gem_indexes(value: Array[int]) -> void:
-		correct_gem_indexes = value
-		reveal_gems(gems_revealed)
+	correct_gem_indexes = value
+	update_gem_reveal_state()
+
+func set_selected_gem_indexes(value: Array[int]) -> void:
+	selected_gem_indexes = value
+	update_gem_reveal_state()
 
 func set_tile_size(value: int) -> void:
 	tile_size = min(Gem.max_tile_size, value)
@@ -52,6 +60,7 @@ func update_nodes() -> void:
 	update_grid_columns_count()
 	update_tile_size()
 	redraw_grid()
+	update_gem_reveal_state()
 
 func update_grid_columns_count() -> void:
 	if not is_node_ready(): return
@@ -63,6 +72,24 @@ func update_tile_size() -> void:
 	var margin = margin_container.get_theme_constant('margin_left') + margin_container.get_theme_constant('margin_right')
 	var free_space = size.x - margin - gap * (grid_size.x - 1)
 	tile_size = free_space / grid_size.x
+
+func update_gem_reveal_state() -> void:
+	for index in gems.size(): 
+		var gem = gems[index]
+		
+		match reveal_type:
+			RevealType.NONE: gem.state = Gem.GemState.HIDDEN
+			RevealType.ALL: gem.state = Gem.GemState.FOUND if index in correct_gem_indexes else Gem.GemState.EMPTY
+			RevealType.SELECTED: 
+				if index not in selected_gem_indexes: gem.state = Gem.GemState.HIDDEN
+				elif index in correct_gem_indexes: gem.state = Gem.GemState.FOUND
+				else: gem.state = Gem.GemState.WRONG
+			RevealType.SELECTED_AND_CORRECT: 
+				if index in selected_gem_indexes: 
+					if index in correct_gem_indexes: gem.state = Gem.GemState.FOUND
+					else: gem.state = Gem.GemState.WRONG
+				elif index in correct_gem_indexes: gem.state = Gem.GemState.MISSED
+				else: gem.state = Gem.GemState.HIDDEN
 
 func redraw_grid() -> void:
 	if not is_node_ready(): return
@@ -78,17 +105,18 @@ func redraw_grid() -> void:
 		item.gem_clicked.connect(gem_clicked.emit.bind(n))
 		item.tile_size = tile_size
 		item.randomize_icon()
-		
-	reveal_gems(gems_revealed)
 
-func reveal_gems(value: bool = true) -> void: gems_revealed = true
-func hide_gems() -> void: gems_revealed = false
 func disable_gems() -> void: for gem in gems: gem.disable()
 func enable_gems() -> void: for gem in gems: gem.enable()
 
 func call_with_delay(fun: Callable, delay_time: float) -> void:
 	await get_tree().create_timer(delay_time).timeout
 	fun.call()
+
+func instnant_fade() -> void:
+	animation_player.play("fade")
+	animation_player.seek(animation_player.current_animation_length)
+	animation_player.stop()
 
 func fade(value: bool) -> Signal:
 	if (value):
