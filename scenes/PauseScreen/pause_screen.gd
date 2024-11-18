c6d236e054d4986f62af124c2df1420c53acb533
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-11-19 00:04:15 +0300

    add pause screen scaffold

---
+++ scenes/PauseScreen/pause_screen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var header: Label = %Header
+@onready var resume_button: Button = %ResumeButton
+@onready var how_to_play_button: Button = %HowToPlayButton
+@onready var restart_button: Button = %RestartButton
+@onready var exit_game_button: Button = %ExitGameButton
+@onready var sound_button: Button = %SoundButton
+@onready var sound_switch: CheckButton = %SoundSwitch
+@onready var vibration_button: Button = %VibrationButton
+@onready var vibration_switch: CheckButton = %VibrationSwitch
